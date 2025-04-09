7beb487e0cdd2cef6da0c395e7f243b77e226349
Maym-Tekstil <maym.tekstil1@gmail.com>
2025-04-09 21:00:48 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ let cart = [];
 
 function addToCart(product, price) {
   cart.push({ product, price });
-  alert(`${product} added to cart!`);
+  alert(`${product} تمت إضافته إلى السلة!`);
 }
 
 function checkout() {
   if (cart.length === 0) {
-    alert("Your cart is empty!");
+    alert("سلة التسوق فارغة!");
     return;
   }
 
   let total = cart.reduce((sum, item) => sum + item.price, 0);
-  alert(`You have ${cart.length} items.\nTotal: $${total.toFixed(2)}\nThanks for shopping!`);
-  cart = []; // Clear the cart
+  alert(`عدد المنتجات: ${cart.length}\nالمجموع: $${total.toFixed(2)}\nشكرًا لتسوقك من Maym!`);
+  cart = [];
 }
